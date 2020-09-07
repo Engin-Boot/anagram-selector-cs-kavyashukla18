a16a8cddb343a9660abffd58e27d635d02f8ec33
kavyashukla18 <69622475+kavyashukla18@users.noreply.github.com>
2020-09-07 15:21:34 +0530

    Update Anagram.cs

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -5,42 +5,51 @@ namespace Anagram
 {
     public class AnagramSelector
     {
-        public bool CheckWordsAreAnagram(string word1, string word2)
+        int[] hashOfWord1 = new int[257];
+
+        public string RemoveSpaceFromWord(string word)
         {
-            word1.ToLower();
-            word2.ToLower();
-            int[] hashOfWord1 = new int[257];
+            return word.Replace(" ", string.Empty);
+        }
+        public void MapEachWord(string word1)
+        {
             Array.Clear(hashOfWord1, 0, hashOfWord1.Length);
-            for(int iterateWord1 = 0; iterateWord1 < word1.Length; iterateWord1++)
+            for (int iterateWord1 = 0; iterateWord1 < word1.Length; iterateWord1++)
             {
-                if(word1[iterateWord1] != ' '&& word1[iterateWord1] >= 'a' && word1[iterateWord1] <= 'z')
+                if (word1[iterateWord1] >= 'a' && word1[iterateWord1] <= 'z')
                 {
                     hashOfWord1[word1[iterateWord1] - 'a']++;
                 }
-                
-            }
 
-            for(int iterateWord2 = 0; iterateWord2 < word2.Length; iterateWord2++)
-            {
-                if(hashOfWord1[word2[iterateWord2] - 'a'] > 0)
-                {
-                    hashOfWord1[word2[iterateWord2] - 'a']--;
-                }
-                else
-                {
-                    return false;
-                }
             }
-            return true;
         }
         public bool WordPairIsAnagram(string word1, string word2) {
             //Insert the correct implementation here
+
+            word1.ToLower();
+            word2.ToLower();
+
+            word1 = RemoveSpaceFromWord(word1);
+            word2 = RemoveSpaceFromWord(word2);
+            MapEachWord(word1);
+
             if (word1.Length != word2.Length)
                 return false;
-            else if (CheckWordsAreAnagram(word1, word2))
-                return true;
             else
-            return false;
+            {
+                for (int iterateWord2 = 0; iterateWord2 < word2.Length; iterateWord2++)
+                {
+                    if (hashOfWord1[word2[iterateWord2] - 'a'] > 0)
+                    {
+                        hashOfWord1[word2[iterateWord2] - 'a']--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
         public List<string> SelectAnagrams(string word, List<string> candidates) {
             //Insert the correct implementation here
